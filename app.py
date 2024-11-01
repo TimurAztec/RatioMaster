4b2afb70757edacec96626499a58a4c855f03164
TimurAztec <timotiaztec@gmail.com>
2024-11-01 23:22:12 +0200

    changed app.py to run on host

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from app import app
 from livereload import Server
 
-if __name__ == '__main__':
-    server = Server(app.wsgi_app)
-    server.serve()
+# if __name__ == '__main__':
+#     server = Server(app.wsgi_app)
+#     server.serve()
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
